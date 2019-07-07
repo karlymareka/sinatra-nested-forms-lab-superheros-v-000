c079e10f3c3dc319a4646260cfd4ece197b10bc8
Karly Mareka <karly.mareka@gmail.com>
2019-07-07 20:47:45 +0000

    Automatically backed up by Learn

---
+++ app/models/team.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class Team
+  
+  attr_accessor :name, :weight, :height
+  @@all = []
+  
+  def initialize(params)
+    @name = params[:name] 
+    @weight = params[:weight] 
+    @height = params[:height]
+    @@all << self 
+  end 
+   
+  def self.all 
+    @@all 
+  end 
+  
+end
