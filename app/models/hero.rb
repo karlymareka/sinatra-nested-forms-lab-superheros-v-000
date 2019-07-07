99519590efb45667471622e2e46fe6236c896492
Karly Mareka <karly.mareka@gmail.com>
2019-07-07 20:50:04 +0000

    Automatically backed up by Learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -6,8 +6,8 @@ class Hero
   
   def initialize(args)
     @name = args[:name]
-    @type = args[:type]
-    @booty = args[:booty] 
+    @type = args[:power]
+    @booty = args[:bio] 
     @@all << self 
   end 
    
