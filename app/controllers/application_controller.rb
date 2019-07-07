f82e0eccfe1b279056e8d980cbd6e263a24fa447
Karly Mareka <karly.mareka@gmail.com>
2019-07-07 21:07:36 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,8 +11,8 @@ class App < Sinatra::Base
   post '/teams' do
     @team = Team.new(params[:team])
     @hero_1 = Hero.new(params[:team][:members][0])
-    @hero_2 = Hero.new(params)
-    @hero_3 = Hero.new(params)
+    @hero_2 = Hero.new(params[:team][:members][1])
+    @hero_3 = Hero.new(params[:team][:members][2])
     erb :team 
   end 
   
